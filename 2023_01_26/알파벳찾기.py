b869dd5966153cdc1f7e80829f7b61a832a5623f
Hanjjay <54357090+Hanjjay@users.noreply.github.com>
2023-01-26 16:05:46 +0900

    알파벳찾기.py추가
    https://www.acmicpc.net/problem/10809

---
+++ 2023_01_26/알파벳찾기.py
@@ -0,0 +1,14 @@
+ip = str(input())
+op = ""
+alp = {}
+for i in range(97, 123):
+    alp[str(chr(i))] = -1
+
+for i in ip:
+    alp[i] = ip.find(i)
+
+for i in list(alp.values()):
+    op += str(i)
+    op += " "
+
+print(op)

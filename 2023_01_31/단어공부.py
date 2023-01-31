4d448040dcbf2d8858ae0f6646f026fd9649269d
Hanjjay <54357090+Hanjjay@users.noreply.github.com>
2023-01-31 21:18:02 +0900

    2023_01_31 문제제출
    https://www.acmicpc.net/problem/1157
    
    https://www.acmicpc.net/problem/1152
    
    https://www.acmicpc.net/problem/2908

---
+++ 2023_01_31/단어공부.py
@@ -0,0 +1,15 @@
+ip = str(input())
+ip.upper()
+op = ""
+alp = {}
+for i in range(97, 123):
+    alp[str(chr(i))] = -1
+
+for i in ip:
+    alp[i] = ip.find(i)
+
+for i in list(alp.values()):
+    op += str(i)
+    op += " "
+
+print(op)
